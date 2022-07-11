0cc41afdbb336b732f0d0c61a14a6880850b12ac
SirDubovskij <SirDubovskij@gmail.com>
2022-07-11 11:14:30 +0300

    task3 commit 3

--- Task3_sql/task3sql/SQLqueries.sql
+++ Task3_sql/task3sql/SQLqueries.sql
@@ -1,11 +1,11 @@
--- task1
+-- _______________task1_________________
 
 -- SELECT category_id, COUNT(film_id)
 -- FROM film_category
 -- GROUP BY category_id
 -- ORDER BY COUNT(film_id) DESC;
 
--- task2
+-- _________________task2_________________
 
 -- SELECT first_name, last_name, SUM(rental_duration)
 -- FROM film
@@ -15,7 +15,7 @@
 -- ORDER BY SUM(rental_duration) DESC
 -- LIMIT 10;
 
--- task3
+-- _________________task3_________________
 
 -- SELECT name , SUM(amount)
 -- FROM category
@@ -28,7 +28,7 @@
 -- ORDER BY SUM(amount) DESC
 -- LIMIT 1;
 
--- task4
+-- _________________task4_________________
 
 -- SELECT title
 -- FROM film
@@ -36,7 +36,7 @@
 -- GROUP BY title
 -- ORDER BY title;
 
--- task5
+-- _________________task5_________________
 
 -- WITH temp_table AS
 --     (SELECT first_name, last_name, COUNT(f.film_id) as film_counter
@@ -57,31 +57,14 @@
 --     LIMIT 3)
 -- ORDER BY film_counter DESC, first_name, last_name;
 
--- task6 haven't done yet
+-- _________________task6_________________
 
--- SELECT address_id,
--- CASE active
---     WHEN 1 THEN COUNT(active)
---     WHEN 0 THEN COUNT(active)
--- END AS counter
--- FROM customer
--- GROUP BY address_id, active
-
--- CASE active
---     WHEN 1 THEN aactive+1
---     WHEN 0 THEN inactive
--- END AS counter
-
--- WITH temp_table AS
---     (SELECT city.city
---     FROM city
---     JOIN address a on city.city_id = a.city_id
---     JOIN customer c on a.address_id = c.address_id
---     GROUP BY city.city
---     ORDER BY city)
--- ALTER TABLE temp_table ADD COLUMN aactive INT
--- ALTER TABLE temp_table ADD COLUMN inactive INT
-
+-- SELECT city.city, COUNT(CASE WHEN active =1 THEN 1 ELSE 0 END) AS Active, COUNT(CASE WHEN active =0 THEN 1 ELSE 0 END) AS Inactive
+-- FROM city
+-- JOIN address a ON city.city_id = a.city_id
+-- JOIN customer c ON a.address_id = c.address_id
+-- GROUP BY city.city
+-- ORDER BY Inactive DESC;
 
 -- SELECT city.city, COUNT(active) AS active1, COUNT(active) AS active0
 -- FROM city
@@ -92,8 +75,8 @@
 -- ORDER BY active1 DESC;
 --
 
--- task7
-
+-- _________________task7_________________
+--
 -- (SELECT name, SUM(rental_duration)
 -- FROM category
 -- JOIN film_category fc on category.category_id = fc.category_id
@@ -107,7 +90,9 @@
 -- GROUP BY name
 -- ORDER BY SUM(rental_duration) DESC
 -- LIMIT 1)
+--
 -- UNION ALL
+--
 -- (SELECT name, SUM(rental_duration)
 -- FROM category
 -- JOIN film_category fc on category.category_id = fc.category_id
